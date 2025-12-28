6d7186f2c3855ae7926acba846fdf63ee49dbbcf
Eino Waldén <eino.a.walden@gmail.com>
2025-12-28 20:41:04 +0200

    Fix formatting

--- ruuturetki-frontend/src/App.tsx
+++ ruuturetki-frontend/src/App.tsx
@@ -4,8 +4,8 @@ import {
   Navigate,
   useNavigate,
 } from 'react-router-dom'
-import 'leaflet/dist/leaflet.css' 
-import { MapContainer, WMSTileLayer} from 'react-leaflet'
+import 'leaflet/dist/leaflet.css'
+import { MapContainer, WMSTileLayer } from 'react-leaflet'
 import { Button } from 'react-bootstrap'
 import { getRandomLatLng } from './components/Game'
 import { useState } from 'react'
@@ -17,25 +17,20 @@ import PlayModal from './components/modals/PlayModal'
 import Practice from './components/Practice'
 import L from 'leaflet'
 
-function StartMenu({setGameSettings}:{setGameSettings: Function}) {
+function StartMenu({ setGameSettings }: { setGameSettings: Function }) {
   const [showPlayModal, setPlayModal] = useState(false)
-  const bg_pos = getRandomLatLng()
-
-
+  const navigate = useNavigate()
 
   const handleClosePlay = () => setPlayModal(false)
   const handleShowPlay = () => setPlayModal(true)
 
-
-  const navigate = useNavigate()
-
-	const wmsOptions: L.WMSOptions = {
-		version: '1.1.1.1',
-		layers: 'avoindata:Ortoilmakuva_2019_20cm',
-    //layers: 'avoindata:Ortoilmakuva',
-		format:'image/png',
-		transparent: false,
-	};
+  const bg_pos = getRandomLatLng()
+  const wmsOptions: L.WMSOptions = {
+    version: '1.1.1.1',
+    layers: 'avoindata:Ortoilmakuva_2019_20cm',
+    format: 'image/png',
+    transparent: false,
+  }
   const mapOptions: L.MapOptions = {
     center: bg_pos,
     zoom: 17,
@@ -44,14 +39,15 @@ function StartMenu({setGameSettings}:{setGameSettings: Function}) {
     boxZoom: false,
     doubleClickZoom: false,
     dragging: false,
-  };
+  }
+
   return (
     <>
       <PlayModal
         show={showPlayModal}
         handleClosePlay={handleClosePlay}
         setGameSettings={setGameSettings}
-        />
+      />
       <MapContainer id="map" {...mapOptions}>
         <WMSTileLayer
           url="https://kartta.hel.fi/ws/geoserver/avoindata/wms?"
@@ -59,49 +55,49 @@ function StartMenu({setGameSettings}:{setGameSettings: Function}) {
           {...wmsOptions}
         />
       </MapContainer>
+
       <div id="menu-title">
-          Ruuturetki
+        Ruuturetki
       </div>
+
       <div id="start-menu" className="d-grid gap-2">
         <Button
           variant="dark"
           size="lg"
           onClick={() => handleShowPlay()}
-          >
+        >
           play
         </Button>
         <Button
           variant="dark"
           size="lg"
           onClick={() => navigate("/practice")}
-          >
+        >
           practice
         </Button>
-
       </div>
     </>
   )
 }
-
 
 function App() {
   const htmlElement = document.querySelector('html')
   if (htmlElement) {
     htmlElement.setAttribute('data-bs-theme', 'dark')
   }
-  const [gameSettings, setGameSettings] = 
+  const [gameSettings, setGameSettings] =
     useState<GameSettings>({
       map: 'avoindata:Ortoilmakuva_2019_20cm',
       year: 2019
-  })
+    })
 
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/game" element={<Game gameSettings={gameSettings}/>} />
+          <Route path="/game" element={<Game gameSettings={gameSettings} />} />
           <Route path="/practice" element={<Practice />} />
-          <Route path="/" element={<StartMenu setGameSettings={setGameSettings}/>} />
+          <Route path="/" element={<StartMenu setGameSettings={setGameSettings} />} />
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </Router>
