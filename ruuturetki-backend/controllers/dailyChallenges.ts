441d60dcdd002358803bfb020203c1e90bd9ae1e
Eino Waldén <eino.a.walden@gmail.com>
2026-02-20 00:19:50 +0200

    Add daily challenges to backend

---
+++ ruuturetki-backend/controllers/dailyChallenges.ts
@@ -0,0 +1,39 @@
+import express from "express";
+const dailyChallengesRouter = express.Router();
+import DailyChallenge from "../models/DailyChallenge.js";
+import { z } from "zod";
+
+const newDailyChallengeSchema = z.object({
+  date: z.string(),
+  dailyChallenge: z.object({
+    id: z.number(),
+    zoom: z.number(),
+    draggable: z.boolean(),
+    latlng: z.object({ lat: z.number(), lng: z.number() }),
+  }),
+  maplayer: z.string(),
+});
+
+dailyChallengesRouter.get("/", (_request, response) => {
+  void DailyChallenge.find({}).then((dailyChallenges) => {
+    response.json(dailyChallenges);
+  });
+});
+
+dailyChallengesRouter.post("/", (request, response) => {
+  try {
+    const newDailyChallenge = newDailyChallengeSchema.parse(request.body);
+    const dailyChallenge = new DailyChallenge({
+      date: newDailyChallenge.date,
+      dailyChallenge: newDailyChallenge.dailyChallenge,
+      maplayer: newDailyChallenge.maplayer,
+    });
+    void dailyChallenge.save().then((saved) => {
+      response.status(201).json(saved);
+    });
+  } catch (error) {
+    response.status(400).send(error);
+  }
+});
+
+export default dailyChallengesRouter;
